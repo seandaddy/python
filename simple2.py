e01af306eea7a643a2bf2319047aaa8d0ddf5287
seandaddy <seandaddy@gmail.com>
2020-02-06 19:17:25 +0900

    new

---
+++ simple2.py
@@ -0,0 +1,18 @@
+a=1
+b=1
+print(a==b)
+print(1==2)
+print(1>2)
+print(1<2)
+print(True)
+print(False)
+
+input = 33
+real_egoing = 11
+real_k8805 = "ab"
+if real_egoing == input:
+  print("Hello!, egoing")
+elif real_k8805 == input:
+  print("Hello!, k8805")
+else:
+  print("Who are you?")
