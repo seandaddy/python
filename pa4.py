6f966dfb7d8c1c1d2fc20d97f6157c48cee514f1
Sang-Yong Oh <seandaddy@gmail.com>
2024-09-29 19:05:25 -0400

    1

--- pa4.py
+++ pa4.py
@@ -1,6 +1,6 @@
-import os
+# import os
 import pandas as pd
-import numpy as np
+# import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
